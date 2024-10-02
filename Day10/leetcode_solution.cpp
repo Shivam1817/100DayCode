d44500301c0eb4f993fedb11ba3ee115f8fdffe4
Shivam <99579186+Shivam1817@users.noreply.github.com>
2024-10-02 10:57:04 +0530

    Create leetcode_solution.cpp

---
+++ Day10/leetcode_solution.cpp
@@ -0,0 +1,23 @@
+//1331. Rank Transform of an Array
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        vector<int> a = arr;
+        sort(a.begin(),a.end());
+        unordered_map<int,int>mp;
+        int i = 0 , n = arr.size() , rank = 1;
+        while(i<n){
+            if(i==0){}
+            else if(a[i]==a[i-1]){}
+            else rank++;
+            mp[a[i]] = rank;
+            i++;
+        }
+        for(auto &x:arr){
+            x=mp[x];
+        }
+        return arr;
+    }
+};
