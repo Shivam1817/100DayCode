e47248f5cf0960afb09428b8a81f405b60cebfdd
Shivam <99579186+Shivam1817@users.noreply.github.com>
2024-10-17 17:01:05 +0530

    Create leetcode_solution.cpp

---
+++ Day25/leetcode_solution.cpp
@@ -0,0 +1,21 @@
+//670. Maximum Swap
+
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string st = to_string(num);
+        int n = st.size();
+        priority_queue<pair<char,int>> pq;
+        int cnt = 1;
+        for(int i = 0 ; i < n ; i++) pq.push({st[i],i});
+        for(int j = 0; j < n; j++)
+        {
+            while(! pq.empty() && pq.top().second < j) pq.pop();
+            if(pq.top().first > st[j]){
+                swap(st[j], st[pq.top().second]);
+                break;
+            }            
+        }
+        return stoi(st);
+    }
+};
