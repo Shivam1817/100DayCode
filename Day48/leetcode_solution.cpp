2d8d587d4a69bb26b837c74ef4f5ff00de367f37
Shivam <99579186+Shivam1817@users.noreply.github.com>
2024-11-09 22:19:25 +0530

    Create leetcode_solution.cpp
    3133. Minimum Array End

---
+++ Day48/leetcode_solution.cpp
@@ -0,0 +1,10 @@
+//3133. Minimum Array End
+
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long num = x;
+        for(int i = 1; i < n; i++) num = (num+1)|x;
+        return num;
+    }
+};
