02ca94adad4925e9cacb8ea739f6d9ae10469966
Shivam <99579186+Shivam1817@users.noreply.github.com>
2024-11-12 12:25:08 +0530

    Create gfg_solution.cpp

---
+++ Day51/gfg_solution.cpp
@@ -0,0 +1,16 @@
+//Meeting Rooms
+
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Your Code Here
+        sort(arr.begin(),arr.end());
+        for(int i=1; i < arr.size() ; i++){
+                if(arr[i-1][1] > arr[i][0]){
+                    return false;
+                
+            }
+        }
+        return true;
+    }
+};
