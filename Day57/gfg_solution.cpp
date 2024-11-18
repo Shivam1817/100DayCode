38351cd3286b211d01b0103303de8da85fc4c4b7
Shivam <99579186+Shivam1817@users.noreply.github.com>
2024-11-18 20:46:17 +0530

    Create gfg_solution.cpp

---
+++ Day57/gfg_solution.cpp
@@ -0,0 +1,16 @@
+//Rotate Array
+
+class Solution {
+  public:
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+        int n = arr.size();
+        d = d%n;
+        if(d == 0) return;
+        reverse(arr.begin(),arr.begin()+d);
+        reverse(arr.begin()+d,arr.end());
+        reverse(arr.begin(),arr.end());
+    }
+};
