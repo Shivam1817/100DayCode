2bbfdba0d639276cc1fc957d77865d5698a26d77
Shivam <99579186+Shivam1817@users.noreply.github.com>
2024-11-18 20:43:49 +0530

    Create leetcode_solution.cpp
    1652. Defuse the Bomb

---
+++ Day57/leetcode_solution.cpp
@@ -0,0 +1,24 @@
+//1652. Defuse the Bomb
+
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> ans(n,0);
+        if(k>0){
+            for(int i = 0; i < n;i++){
+                for(int j = 1 ; j <= k ;j++){
+                    ans[i] += code[(i+j)%n];
+                }
+            }
+        }
+        if (k < 0) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 1; j <= -k; j++) {
+                    ans[i] += code[(i - j + n) % n];
+                }
+            }
+        }
+        return ans;
+    }
+};
